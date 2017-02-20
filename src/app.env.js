19f581ef3a88ec86bc887e47a21ce53573ce9ec3
Ruy Garcia <rrgarciach@gmail.com>
2017-02-19 20:23:42 -0700

    Fixed server URL

--- src/app.env.js
+++ src/app.env.js
@@ -1,4 +1,4 @@
 export default {
-    PARSE_SERVER_URL: 'http://localhost:9090'
-    // PARSE_SERVER_URL: 'https://saturn-parse-server-dev.herokuapp.com/parse'
+    // PARSE_SERVER_URL: 'http://localhost:9090'
+    PARSE_SERVER_URL: 'https://saturn-parse-server-dev.herokuapp.com'
 };
